d4ab7c31a01a51489bd35eb23c1338c8694f1dba
Ty <tytrusty@utexas.edu>
2017-11-26 16:41:58 -0600

    added config header. just the bare necessities

---
+++ src/config.h
@@ -0,0 +1,10 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+namespace config
+{
+    const int N =  14;
+    const float viscosity = 0.99999f;
+    const float diffusion = 0; //100.999f;
+    const float time_step = 0.125f;
+}
+#endif // CONFIG_H
