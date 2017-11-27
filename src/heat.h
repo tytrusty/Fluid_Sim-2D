dd8531b6b5c9bdca159c2f273694733cf33caeae
Ty <tytrusty@utexas.edu>
2017-11-27 13:55:59 -0600

    heat me baby one more time

---
+++ src/heat.h
@@ -0,0 +1,17 @@
+#ifndef HEAT_H
+#define HEAT_H
+
+#include <glm/glm.hpp>
+#include <vector>
+
+namespace heat
+{
+    static float radius;
+    const int circle_vertices = 50;
+    const float TWO_PI = glm::pi * 2.0f;
+    std::vector<glm::vec2> draw_boundary()
+    {
+    
+    }
+}
+#endif // HEAT_H
