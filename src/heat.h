8d7b4010cea0ea302d974a0b502f72456e1cad3f
Ty Trusty <tytrusty@utexas.edu>
2017-11-28 00:10:09 -0600

    it's gettin hot in here

--- src/heat.h
+++ src/heat.h
@@ -3,18 +3,26 @@
 
 #include <glm/glm.hpp>
 #include <vector>
+#include <ctime>
 #include "math.h"
+#include "config.h"
+
 namespace heat
 {
     const int circle_vertices = 100;
-    static float radius = 0.1;
-    const int x = 10;
-    const int y = 10;
+    static float radius = 0.01f;
     const float TWO_PI = M_PI * 2.0f;
-    std::vector<glm::vec2> draw_boundary(float x = 0.5, float y = 0.5)
+
+    static clock_t begin = clock();
+
+    std::vector<glm::vec2> draw_boundary(float x = 0.8, float y = 0.8)
     {
+        clock_t end = clock();
+        double elapsed = (double(end - begin) / CLOCKS_PER_SEC);
+        begin = end;
+        radius += (elapsed / config::time_step) * 0.005;
+
         std::vector<glm::vec2> boundary;
-        //boundary.push_back(glm::vec2(x, y));
         for (int i = 0; i <= circle_vertices; ++i) {
             glm::vec2 vertex;
             vertex[0] = x + (radius * std::cos(i * TWO_PI / circle_vertices)); 
